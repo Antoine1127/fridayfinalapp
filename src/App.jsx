34c1be23a6b548521eb181a588af3acc9f164d08
Antoine1127 <s_antoine.mehouelley@ousd.org>
2026-05-18 23:36:55 -0700

    yo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Page1 from "./components/Page1";
 import Page2 from "./components/Page2";
 import Page3 from "./components/Page3";
 import About_page from "./components/About_page";
-//import styles from "./Intro_page.module.css"; 
 
 
 
